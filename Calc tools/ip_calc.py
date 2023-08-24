16a2176a1c6e6115f3d8bf7f8da5377ebc28be6a
Mr.Salih <salihbaz07@gmail.com>
2023-08-24 15:00:46 +0300

    ip calc edit

--- Calc tools/ip_calc.py
+++ Calc tools/ip_calc.py
@@ -6,12 +6,24 @@ def calculate_network_broadcast(ip, subnet_mask):
     broadcast = network.broadcast_address
     return network, broadcast
 
+def calculate_ip_count(subnet_mask):
+    ip_count = 2 ** (32 - sum(bin(int(x)).count('1') for x in subnet_mask.split('.')))
+    return ip_count - 2
+
 ip_address = input("IP Adresini giriniz: ")
 subnet_mask = input("Subnet Mask giriniz: ")
 
+binary_ip = '.'.join(format(int(octet), '08b') for octet in ip_address.split('.'))
+binary_subnet_mask = '.'.join(format(int(octet), '08b') for octet in subnet_mask.split('.'))
+
 network, broadcast = calculate_network_broadcast(ip_address, subnet_mask)
-print(f"IP Adresinin Ağı(Network): {network}")
-print(f"IP Adresinin Yayın Adresi(Broadcast): {broadcast}")
+ip_count = calculate_ip_count(subnet_mask)
+
+print(f"IP Adresi Binary: {binary_ip}")
+print(f"Subnet Maskesi Binary: {binary_subnet_mask}")
+print(f"Ağ Adresi: {network}")
+print(f"Yayın Adresi: {broadcast}")
+print(f"Verilen IP Adresi ve Alt Ağ Maskesi ile Alınabilecek Toplam Üye IP Adresi Sayısı: {ip_count} + 2")
 
 while True:
     input("")
